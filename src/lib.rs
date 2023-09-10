03d6b92931397090bb867af05a41671fc9ed4ed8
ahmadreza <djahmadfire@gmail.com>
2023-09-10 14:24:50 +0330

    some improvements on code

--- src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub fn ipfinder() -> Vec<(String, std::net::IpAddr)> {
-    local_ip_address::list_afinet_netifas().expect("Error getting ip")
-}
--- src/server_btn.rs
+++ src/server_btn.rs
@@ -1,5 +1,4 @@
 use crate::server::server;
-use redstone::ipfinder;
 
 use cursive::{
     align::HAlign,
@@ -14,18 +13,18 @@ pub fn server_btn(s: &mut cursive::Cursive) {
     // Create selectview
     let mut select = SelectView::new().h_align(HAlign::Center).autojump();
     // Get list of IPs and add to selectview
-    select.add_all(ipfinder());
+    select.add_all(local_ip_address::list_afinet_netifas().expect("Error getting ip"));
     // Add functionality to selectview on select
     select.set_on_submit(|s, c| {
+        // IP + Port = IP:Port
+        let ip_port = format!("{c}:1140");
         s.pop_layer();
         // Show the IP listening to
         s.add_layer(
-            TextView::new(format!("Server listening on {c}:1140"))
+            TextView::new(format!("Server listening on {ip_port}"))
                 .h_align(HAlign::Center)
                 .fixed_height(5),
         );
-        // IP + Port = IP:Port
-        let ip_port = format!("{}:1140", c.clone());
         // Run server thread
         tokio::spawn(async { server(ip_port).await });
     });
